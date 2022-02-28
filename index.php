23793453b88c6345ae1f4d21eeaf1c3212f3c387
Damianp99 <damianperez200199@gmail.com>
2022-02-28 15:22:45 +0100

    starting

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Consigli:
+Teniamo a mente le differenze tra array associativo ed array con indici numerici
+Ricordiamo che possiamo inserire tag all'interno delle stringhe di PHP, saranno convertiti in HTML quando faremo echo
+Concentriamoci sulle FAQ e lasciamo per ultimo header e footer (e il loro CSS)
+Lasciamo per ultimo header e footer (e il loro CSS) e concentriamoci sulle FAQ
+Prima facciamo le FAQ e solo dopo header e footer.
+BONUS:
+Inserire le voci di menu dell'header tramite un array di PHP (i link non devono funzionare)
+
+*/
+
+$faq = [
+    []
+]
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Google FAQ</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+</body>
+
+</html>
